9ac671de9bc380a589d3156dc0c4e2fcee29a53c
mccclxxvii <pavelpalamarchuk@gmail.com>
2020-02-21 17:36:01 +0200

    Update quiz21.py

--- quiz21.py
+++ quiz21.py
@@ -5,6 +5,7 @@ import pymysql
 import pymysql.cursors
 from telebot import types
 from difflib import SequenceMatcher
+import sys
 
 def getUser(con,cur,bot,message):
 	userID = -1
@@ -18,7 +19,6 @@ def getUser(con,cur,bot,message):
 									+str(message.from_user.first_name).replace('\'','\'\'')+"','"
 									+str(message.from_user.last_name).replace('\'','\'\'')+"','"
 									+str(message.from_user.username).replace('\'','\'\'')+"')")
-		con.commit()
 		bot.send_message(message.from_user.id,'Привет, '+str(user[0]['FIRST_NAME']))
 		cur.execute("SELECT ID,FIRST_NAME "
 					+"FROM T_USER "
@@ -26,7 +26,7 @@ def getUser(con,cur,bot,message):
 		user = cur.fetchall()
 		bot.send_message(message.from_user.id,'Привет, '+str(user[0]['FIRST_NAME']))
 	else:
-		cur.execute("UPDATE T_USER "+
+		cur.execute("UPDATE T_USER "
 					+"SET FIRST_NAME='"+str(message.from_user.first_name).replace('\'','\'\'')+"',"
 					+"LAST_NAME='"+str(message.from_user.last_name).replace('\'','\'\'')+"',"
 					+"NICK_NAME='"+str(message.from_user.username).replace('\'','\'\'')+"' "
@@ -40,13 +40,11 @@ def getUser(con,cur,bot,message):
 					+"SELECT "+str(user[0]["ID"])+",ID "
 					+"FROM T_BOT "
 					+"WHERE NAME='@QuizXIVbot'")
-	con.commit()
 	cur.execute("SELECT * FROM T_USER_BOT ub,T_BOT b "
 				+"WHERE ub.USER_ID="+str(user[0]["ID"])
 				+" AND ub.BOT_ID=b.ID"
 				+" AND b.NAME='@QuizXIVbot'")
 	user = cur.fetchall()
-	con.commit()
 	if user[0]['STATUS_CD']=="LOCKED":
 		bot.send_message(message.from_user.id,'Извините, ваша учетная запись заблокирована')
 	else:
@@ -57,12 +55,11 @@ def getGame(con,cur,userID,bot,tgUserId):
 	cur.execute("SELECT q.*,d.QUESTION "
 				+"FROM T_QUEST_MAIN q "
 				+"LEFT JOIN T_QUEST_DICT d ON q.THIS_QUEST_ID=d.ID "
-				+"WHERE q.USER_BOT_ID="+str(userID)+" AND q.STATUS_CD='Active'")
+				+"WHERE q.USER_BOT_ID="+str(userID))
 	game = cur.fetchall()
 	if len(game)==0:
 		cur.execute("INSERT INTO T_QUEST_MAIN(USER_BOT_ID) "
 					+"values("+str(userID)+")")
-		con.commit()
 		cur.execute("SELECT q.*,d.QUESTION "
 					+"FROM T_QUEST_MAIN q "
 					+"LEFT JOIN T_QUEST_DICT d ON q.THIS_QUEST_ID=d.ID "
@@ -70,17 +67,10 @@ def getGame(con,cur,userID,bot,tgUserId):
 		bot.send_message(tgUserId,'Начнем')
 		game = cur.fetchall()
 	return(game[0])
-	
+
 def getNextQuest(con,cur,game):
 	res = {}
 	mes = []
-	but = ["/next","/add"]
-	cur.execute("UPDATE T_QUEST_MAIN "
-				+"SET THIS_QUEST_ID=null, "
-				+"MODE_CD='Default',"
-				+"LAST_ANSWER_ID=NULL "
-				+"WHERE ID="+str(game['ID']))
-	con.commit()
 	cur.execute("SELECT q.* FROM "
 				+"(SELECT c.*,ROW_NUMBER() over() rwn "
 					+"FROM T_QUEST_DICT c "
@@ -93,21 +83,25 @@ def getNextQuest(con,cur,game):
 	if len(quest)>0:
 		cur.execute("UPDATE T_QUEST_MAIN "
 					+"SET THIS_QUEST_ID ='"+str(quest[0]['ID'])+"', "
-					+"MODE_CD='Exists Question' "
+					+"MODE_CD='Default', "
+					+"STATUS_CD='PreAnswer', "
+					+"LAST_ANSWER_ID=NULL "
 					+"WHERE ID="+str(game['ID']))
-		con.commit()
 		mes.append(str(quest[0]['QUESTION']))
-		but.append("/get")
 	else:
+		cur.execute("UPDATE T_QUEST_MAIN "
+					+"SET STATUS_CD='Default', "
+					+"MODE_CD='Default', "
+					+"THIS_QUEST_ID=NULL, "
+					+"LAST_ANSWER_ID=NULL "
+					+"WHERE ID="+str(game['ID']))
 		mes.append('Вопросы закончились')
 	res['messages']=mes
-	res['buttons']=but
 	return (res)
 
 def userMoveCheck(con,cur,game,answer):
 	res = {}
 	mes = []
-	but = ["/next","/add","/get","/dispute","/why"]
 	cur.execute("select d.ANSWER "
 					+"from T_QUEST_DICT d "
 					+"WHERE d.ID="+str(game['THIS_QUEST_ID'])
@@ -140,6 +134,10 @@ def userMoveCheck(con,cur,game,answer):
 						+str(game['ID'])+","
 						+str(game['THIS_QUEST_ID'])+","
 						+"'"+str(answer)+"')")
+		cur.execute("UPDATE T_QUEST_MAIN "
+						+"SET LAST_ANSWER_ID='"+str(rowId)+"', "
+						+"STATUS_CD='Answer' "
+						+"WHERE ID="+str(game['ID']))
 	else:
 		mes.append("Ответ '"+rightAnswer+"' принят.")
 		cur.execute("INSERT INTO T_QUEST_BEEN(ID,GAME_ID,QUEST_ID,ANSWER,RESULT) VALUES("
@@ -147,41 +145,32 @@ def userMoveCheck(con,cur,game,answer):
 						+str(game['ID'])+","
 						+str(game['THIS_QUEST_ID'])+","
 						+"'"+str(answer)+"','Correct')")
-	cur.execute("UPDATE T_QUEST_MAIN "+
-				+"SET LAST_ANSWER_ID='"+str(rowId)+"', "
-				+"MODE_CD='Exists Answer' "
-				+"WHERE ID="+str(game['ID']))
-	con.commit()
+		cur.execute("UPDATE T_QUEST_MAIN "
+					+"SET LAST_ANSWER_ID='"+str(rowId)+"', "
+					+"STATUS_CD='PostAnswer' "
+					+"WHERE ID="+str(game['ID']))
 	res['messages']=mes
-	res['buttons']=but
 	return (res)
 
-def editMode(con,cur,game,mode):
+def editMode(con,cur,game,new_mode,comment):
 	res = {}
 	mes = []
-	but = ["/cancel"]
 	cur.execute("UPDATE T_QUEST_MAIN "
-				+"SET MODE_CD='"+mode+"' "
+				+"SET MODE_CD='"+new_mode+"' "
 				+"WHERE ID="+str(game['ID']))
-	con.commit()
-	if mode=="Dispute":
-		mes.append('Введите комментарий')
-	else:
-		mes.append('Введите вопрос')
+	mes.append(comment)
 	res['messages']=mes
-	res['buttons']=but
 	return (res)
 
 def getAnswer(con,cur,game):
 	res = {}
 	mes = []
-	but = ["/next","/add","/dispute","/why"]
 	cur.execute("SELECT d.ANSWER "
 				+"FROM T_QUEST_MAIN m, T_QUEST_DICT d "
 				+"WHERE m.THIS_QUEST_ID=d.ID AND m.ID="+str(game['ID']))
 	answer = cur.fetchall()
 	mes.append(str(answer[0]["ANSWER"]))
-	cur.execute("SELECT NVL(MAX(ID),0)+1 ROW_ID "+
+	cur.execute("SELECT NVL(MAX(ID),0)+1 ROW_ID "
 				+"FROM T_QUEST_BEEN")
 	rowId = cur.fetchall()[0]['ROW_ID']
 	cur.execute("INSERT INTO T_QUEST_BEEN(ID,GAME_ID,QUEST_ID,ANSWER,RESULT) VALUES("
@@ -191,17 +180,15 @@ def getAnswer(con,cur,game):
 								+"'"+str(answer[0]["ANSWER"])+"',"
 								+"'Pass')")
 	cur.execute("UPDATE T_QUEST_MAIN "
-				+"SET LAST_ANSWER_ID='"+str(rowId)+"' "
+				+"SET LAST_ANSWER_ID='"+str(rowId)+"', "
+				+"STATUS_CD='PostAnswer' "
 				+"WHERE ID="+str(game['ID']))
-	con.commit()
 	res['messages']=mes
-	res['buttons']=but
 	return (res)
 
 def getComment(con,cur,game):
 	res = {}
 	mes = []
-	but = ["/next","/add","/dispute"]
 	cur.execute("SELECT d.ANSWER,nvl(d.COMMENTS,'Просто так') COMMENTS "
 				+"FROM T_QUEST_MAIN m, T_QUEST_DICT d "
 				+"WHERE m.THIS_QUEST_ID=d.ID AND m.ID="+str(game['ID']))
@@ -209,80 +196,86 @@ def getComment(con,cur,game):
 	for ans in answer:
 		mes.append(str(ans["COMMENTS"]))
 	res['messages']=mes
-	res['buttons']=but
+	cur.execute("UPDATE T_QUEST_BEEN "
+					+"SET COMM_FLG=1 "
+					+"WHERE ID="+str(game['LAST_ANSWER_ID']))
 	return (res)
 
 def cancelOperation(con,cur,game):
 	res = {}
 	mes = []
-	but = ["/next","/add"]
 	cur.execute("UPDATE T_QUEST_MAIN "
 				+"SET MODE_CD='Default' "
 				+"WHERE ID="+str(game['ID']))
-	con.commit()
-	mes.append("Вы можете выбрать следующий вопрос или добавить новый")
+	mes.append("Охрана, отмена")
 	res['messages']=mes
-	res['buttons']=but
 	return (res)
 
 def firstStepAddQuest(con,cur,game,text):
 	res = {}
 	mes = []
-	but = ["/cancel"]
-	cur.execute("UPDATE T_QUEST_MAIN SET MODE_CD='Add Answer',TEMP_STR='"+text.replace('\'','\'\'')+"' WHERE ID="+str(game['ID']))
-	con.commit()
+	cur.execute("UPDATE T_QUEST_MAIN "
+				+"SET MODE_CD='Add Answer',"
+				+"TEMP_STR='"+text.replace('\'','\'\'')+"' "
+				+"WHERE ID="+str(game['ID']))
 	mes.append("Введите ответ (варианты строго через запятую и пробел)")
 	res['messages']=mes
-	res['buttons']=but
 	return (res)
 
 def secondStepAddQuest(con,cur,game,text):
 	res = {}
 	mes = []
-	but = ["/next","/add"]
-	cur.execute("UPDATE T_QUEST_MAIN SET MODE_CD ='Default',TEMP_STR=null WHERE ID="+str(game['ID']))
-	con.commit()
+	cur.execute("UPDATE T_QUEST_MAIN "
+				+"SET MODE_CD ='Default',"
+				+"TEMP_STR=NULL "
+				+"WHERE ID="+str(game['ID']))
 	answers = text.split(', ')
-	cur.execute("SELECT NVL(MAX(ID),0)+1 ROW_ID FROM T_QUEST_DICT")
+	cur.execute("SELECT NVL(MAX(ID),0)+1 ROW_ID "
+				+"FROM T_QUEST_DICT")
 	rowId = cur.fetchall()[0]['ROW_ID']
 	cur.execute("INSERT INTO T_QUEST_DICT(ID,QUESTION,ANSWER,CREATED_BY) VALUES("
 										+str(rowId)+",'"+str(game['TEMP_STR'])+"','"+str(answers[0])+"',"+str(game['USER_BOT_ID'])+")")
 	for i in range(1,len(answers)):
 		cur.execute("INSERT INTO T_QUEST_DICT_VAR (QUEST_ID,VAL) VALUES("+str(rowId)+",'"+str(answers[i])+"')")
-	con.commit()
 	mes.append("Спасибо, [Name]. Вопрос добавлен")
 	res['messages']=mes
-	res['buttons']=but
 	return (res)
 
 def disputeQuest(con,cur,game,text):
 	res = {}
 	mes = []
-	but = ["/next","/add"]
-	cur.execute("UPDATE T_QUEST_MAIN "
-				+"SET MODE_CD='Default' "
-				+"WHERE ID="+str(game['ID']))
-	con.commit()
 	if not game['LAST_ANSWER_ID']:
-		cur.execute("INSERT INTO T_QUEST_BEEN (GAME_ID,QUEST_ID,DISPUTE_FLG,COMMENTS) VALUES ("
-											+str(game['ID'])+","+str(game['THIS_QUEST_ID'])+",1,'"+str(text)+"')")
+		cur.execute("INSERT INTO T_QUEST_BEEN (GAME_ID,QUEST_ID,COMMENTS) VALUES ("
+											+str(game['ID'])+","+str(game['THIS_QUEST_ID'])+",'"+str(text)+"')")
 	else:
 		cur.execute("UPDATE T_QUEST_BEEN "
-					+"SET DISPUTE_FLG=1,COMMENTS='"+str(text)+"' "
+					+"SET COMMENTS='"+str(text)+"' "
 					+"WHERE ID="+str(game['LAST_ANSWER_ID']))
-	con.commit()
-	mes.append("Спасибо, учту")
+	mes.append("Спасибо за отзыв, Ваше мнение очень важно для меня. Вы можете выбрать следующий вопрос или добавить новый")
 	res['messages']=mes
-	res['buttons']=but
+	return (res)
+
+def rateQuest(con,cur,game,text):
+	res = {}
+	mes = []
+	if not re.search('^[1-5]$', str(text)):
+		mes.append("Обнаружены неверные символы. Прошу ввести оценка от 1 до 5")
+	else:
+		cur.execute("UPDATE T_QUEST_MAIN "
+					+"SET MODE_CD='Default' "
+					+"WHERE ID="+str(game['ID']))
+		cur.execute("UPDATE T_QUEST_BEEN "
+					+"SET RATE="+str(text)+" "
+					+"WHERE ID="+str(game['LAST_ANSWER_ID']))
+		mes.append("Спасибо за отзыв. Вы можете выбрать следующий вопрос или добавить новый")
+	res['messages']=mes
 	return (res)
 
 def defaultFunc():
 	res = {}
 	mes = []
-	but = ["/next","/add"]
 	mes.append("Вы можете выбрать следующий вопрос или добавить новый")
 	res['messages']=mes
-	res['buttons']=but
 	return (res)
 
 def main(bot,type,message):
@@ -292,92 +285,158 @@ def main(bot,type,message):
 			con = pymysql.connect(host='176.36.217.49',port=3307,user='TELEGRAM_BOT',password='!1qaZXsw2@',db='NasDB',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
 			cur = con.cursor()
 			try:
-				userID = getUser(con,cur,bot,message)
+				if mode == 'debug':
+					userID = 18
+					tgId = 394246173
+					bot = 'bot'
+					messageText = message
+				else:
+					userID = getUser(con,cur,bot,message)
+					tgId=message.from_user.id
+					messageText = message.text
 				if userID>=0:
-					game = getGame(con,cur,userID,bot,message.from_user.id)
+					game = getGame(con,cur,userID,bot,tgId)
 					func = {
-						"Exists Question":{#есть новый вопрос
-							"next":"getNextQuest(con,cur,game)",#перейти к следующему вопросу
-							"add":"editMode(con,cur,game,'Add Question')",#добавить новый вопрос
-							"get":"getAnswer(con,cur,game)",#узнать ответ
-							"default":"userMoveCheck(con,cur,game,message.text)",#ввод ответа
-							"dispute":"editMode(con,cur,game,'Dispute')",#оспроить вопрос
-							"why":"getComment(con,cur,game)"#почему?
-						},
-						"Exists Answer":{#есть введенный вопрос
-							"next":"getNextQuest(con,cur,game)",#перейти к следующему вопросу
-							"add":"editMode(con,cur,game,'Add Question')",#добавить новый вопрос
-							"get":"getAnswer(con,cur,game)",#узнать ответ
-							"dispute":"editMode(con,cur,game,'Dispute')",#оспроить вопрос
-							"default":"userMoveCheck(con,cur,game,message.text)",#ввод ответа
-							"why":"getComment(con,cur,game)"#почему?
-						},
-						"Add Question":{#режим добавления вопроса
-							"next":"getNextQuest(con,cur,game)",#перейти к следующему вопросу
-							"cancel":"cancelOperation(con,cur,game)",#отменить
-							"default":"firstStepAddQuest(con,cur,game,message.text)"#ввод нового вопроса
-						},
-						"Add Answer":{#режим добавления ответа
-							"next":"getNextQuest(con,cur,game)",#перейти к следующему вопросу
-							"cancel":"cancelOperation(con,cur,game)",#отменить
-							"default":"secondStepAddQuest(con,cur,game,message.text)"#ввод нового ответа
-						},
-						"Dispute":{#режим оспаривания
-							"next":"getNextQuest(con,cur,game)",#перейти к следующему вопросу
-							"cancel":"cancelOperation(con,cur,game)",#отменить
-							"default":"disputeQuest(con,cur,game,message.text)",#ввод комментария
-						},
 						"Default":{
-							"next":"getNextQuest(con,cur,game)",#перейти к следующему вопросу
-							"add":"editMode(con,cur,game,'Add Question')",#добавить новый вопрос
-							"default":"defaultFunc()"#игнорировать
+							"Default":{
+								"next":"getNextQuest(con,cur,game)",#перейти к следующему вопросу
+								"add":"editMode(con,cur,game,'Add Question','Введите вопрос')",#добавить новый вопрос
+								"default":"defaultFunc()"#игнорировать
+							},
+							"Add Question":{#режим добавления вопроса
+								"cancel":"cancelOperation(con,cur,game)",#отменить
+								"default":"firstStepAddQuest(con,cur,game,messageText)"#ввод нового вопроса
+							},
+							"Add Answer":{#режим добавления ответа
+								"cancel":"cancelOperation(con,cur,game)",#отменить
+								"default":"secondStepAddQuest(con,cur,game,messageText)"#ввод нового ответа
+							}
 						},
+						"PreAnswer":{
+							"Default":{
+								"next":"getNextQuest(con,cur,game)",#перейти к следующему вопросу
+								"add":"editMode(con,cur,game,'Add Question','Введите вопрос')",#добавить новый вопрос
+								"get":"getAnswer(con,cur,game)",#узнать ответ
+								"default":"userMoveCheck(con,cur,game,messageText)"#проверить
+							},
+							"Add Question":{#режим добавления вопроса
+								"cancel":"cancelOperation(con,cur,game)",#отменить
+								"default":"firstStepAddQuest(con,cur,game,messageText)"#ввод нового вопроса
+							},
+							"Add Answer":{#режим добавления ответа
+								"cancel":"cancelOperation(con,cur,game)",#отменить
+								"default":"secondStepAddQuest(con,cur,game,messageText)"#ввод нового ответа
+							}
+						},
+						"Answer":{
+							"Default":{
+								"next":"getNextQuest(con,cur,game)",#перейти к следующему вопросу
+								"get":"getAnswer(con,cur,game)",#узнать ответ
+								"add":"editMode(con,cur,game,'Add Question','Введите вопрос')",#добавить новый вопрос
+								"default":"userMoveCheck(con,cur,game,messageText)"#проверить
+							},
+							"Add Question":{#режим добавления вопроса
+								"cancel":"cancelOperation(con,cur,game)",#отменить
+								"default":"firstStepAddQuest(con,cur,game,messageText)"#ввод нового вопроса
+							},
+							"Add Answer":{#режим добавления ответа
+								"cancel":"cancelOperation(con,cur,game)",#отменить
+								"default":"secondStepAddQuest(con,cur,game,messageText)"#ввод нового ответа
+							}
+						},
+						"PostAnswer":{
+							"Default":{
+								"next":"getNextQuest(con,cur,game)",#перейти к следующему вопросу
+								"add":"editMode(con,cur,game,'Add Question','Введите вопрос')",#добавить новый вопрос
+								"why":"getComment(con,cur,game)",#почему?
+								"rate":"editMode(con,cur,game,'Rate','Оцените сложность вопроса от 1 до 5')",
+								"default":"disputeQuest(con,cur,game,messageText)"#комментировать вопрос
+							},
+							"Add Question":{#режим добавления вопроса
+								"cancel":"cancelOperation(con,cur,game)",#отменить
+								"default":"firstStepAddQuest(con,cur,game,messageText)"#ввод нового вопроса
+							},
+							"Add Answer":{#режим добавления ответа
+								"cancel":"cancelOperation(con,cur,game)",#отменить
+								"default":"secondStepAddQuest(con,cur,game,messageText)"#ввод нового ответа
+							},
+							"Rate":{#режим добавления ответа
+								"cancel":"cancelOperation(con,cur,game)",#отменить
+								"default":"rateQuest(con,cur,game,messageText)"#оценка вопроса
+							}
+						}
 					}
 					x = ""
 					try:
-						x = func[game['MODE_CD']][type]
+						x = func[game['STATUS_CD']][game['MODE_CD']][type]
 					except Exception as f:
-						print(game['MODE_CD'],type)
+						print(game['STATUS_CD'],game['MODE_CD'],type)
 					if len(x)==0:
 						result = defaultFunc()
 					else:
-						result = eval(func[game['MODE_CD']][type])
-					markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-					for button in result['buttons']:
-						itembtn = types.KeyboardButton(button)
-						markup.add(itembtn)
+						result = eval(func[game['STATUS_CD']][game['MODE_CD']][type])
+					markup = types.ReplyKeyboardMarkup(row_width=5, resize_keyboard=True)
+					cur.execute("SELECT c.*,b.COMM_FLG,b.RATE "
+								+"from T_QUEST_CONFIG c, T_QUEST_MAIN m "
+								+"LEFT JOIN T_QUEST_BEEN b ON b.ID=m.LAST_ANSWER_ID "
+								+"where m.ID="+str(game['ID'])
+								+" AND c.MODE=m.MODE_CD"
+								+" AND c.STATE=m.STATUS_CD")
+					buttons = cur.fetchall()
+					for button in buttons:
+						if button['COMMAND']=='/rate' and button['RATE']:
+							pass
+						elif button['COMMAND']=='/why' and button['COMM_FLG']==1:
+							pass
+						else:
+							itembtn = types.KeyboardButton(text=button['BUTTON_NAME'])
+							markup.add(itembtn)
 					for mess in result['messages']:
-						bot.send_message(message.from_user.id, mess.replace('[Name]',str(message.from_user.first_name)), reply_markup=markup)
+						if mode == 'debug':
+							print(mess)
+						else:
+							bot.send_message(message.from_user.id, mess.replace('[Name]',str(message.from_user.first_name)), reply_markup=markup)
+				con.commit()
 			except Exception as e:
-				bot.send_message(message.from_user.id,"Ошибка:"+str(e))
+				if mode == 'debug':
+					print("Ошибка:",str(e))
+				else:
+					bot.send_message(message.from_user.id,"Ошибка:"+str(e))
+				con.rollback()
 			cur.close()
 			con.close()
+		else:
+			bot.send_message(message.from_user.id,"Троводятся технические работы")
 	except Exception as e:
 		bot.send_message(message.from_user.id,"Ошибка:"+str(e))
 
-try:
-	bot = telebot.TeleBot("959911904:AAHlPuS8mMdsqQrMmdIG0h5Y9COtyFbCpfs")
-	@bot.message_handler(commands=['next'])
-	def command_next(message):
-		main(bot,'next',message)
-	@bot.message_handler(commands=['add'])
-	def command_add(message):
-		main(bot,'add',message)
-	@bot.message_handler(commands=['dispute'])
-	def command_disp(message):
-		main(bot,'dispute',message)
-	@bot.message_handler(commands=['why'])
-	def command_next(message):
-		main(bot,'why',message)
-	@bot.message_handler(commands=['cancel'])
-	def command_next(message):
-		main(bot,'cancel',message)
-	@bot.message_handler(commands=['get'])
-	def command_next(message):
-		main(bot,'get',message)
-	@bot.message_handler(content_types=['text'])
-	def get_text_messages(message):
-		main(bot,'default',message)
-	bot.polling(none_stop=False, interval=1)
-except Exception as e:
-	print(e)
+mode = 'default'
+if len(sys.argv)>1:
+	if sys.argv[1]=='debug':
+		mode = 'debug'
+	elif sys.argv[1]=='log':
+		mode = 'log'
+if mode == 'debug':
+	main('bot',sys.argv[2],sys.argv[3])
+else:
+	try:
+		bot = telebot.TeleBot("959911904:AAHlPuS8mMdsqQrMmdIG0h5Y9COtyFbCpfs")
+		@bot.message_handler(content_types=['text'])
+		def get_text_messages(message):
+			if message.text =='*Следующий вопрос*':
+				main(bot,'next',message)
+			elif message.text =='*Добавить свой вопрос*':
+				main(bot,'add',message)
+			elif message.text =='*Оценить сложность вопроса*':
+				main(bot,'rate',message)
+			elif message.text =='*Объяснить*':
+				main(bot,'why',message)
+			elif message.text =='*Отменить*':
+				main(bot,'cancel',message)
+			elif message.text =='*Узнать ответ*':
+				main(bot,'get',message)
+			else:
+				main(bot,'default',message)
+		bot.polling(none_stop=False, interval=1)
+	except Exception as e:
+		print(e)
